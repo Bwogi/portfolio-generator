a620b413428b23dc4d683663a3eb6f193cbdf92e
Full Stack Developer <ucberkeley@fulls-air.home>
2021-08-01 00:56:27 -0700

    capture commandline input with process.argv

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ transcriptObj.semID = '1';
 
 console.log(transcriptObj);
 
+console.log('---');
 
 
 
@@ -89,9 +90,19 @@ console.log(`addNumbers: ${addNumbers(2,1)}`);
 console.log(`addTheNos: ${addTheNos(2,1)}`);
 console.log(`addThoseNos: ${addThoseNos(2,1)}`);
 console.log('---');
+console.log('---');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+console.log(profileDataArgs);
+console.log('---');
+
+const profileDataArgsNew = process.argv.slice(0, 2); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+console.log(profileDataArgsNew);
+console.log('---');
+
+const profileDataArgsNewOne = process.argv.slice(0, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+console.log(profileDataArgsNewOne);
+console.log('---');
 
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
@@ -102,3 +113,13 @@ const printProfileData = profileDataArr => {
 printProfileData(profileDataArgs);
 
 console.log('---');
+
+const profile = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+console.log(profile);
+console.log('---');
+
+const printProfile = theArray => console.log(theArray);
+
+
+printProfile(profile);
+// cleaner loops
