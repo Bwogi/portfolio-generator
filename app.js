04c4414226cd6f8b78b4a38674f0d1924c484404
Full Stack Developer <ucberkeley@Fulls-MacBook-Air.local>
2021-08-01 20:50:48 -0700

    template literals and html

--- app.js
+++ app.js
@@ -1,154 +1,209 @@
-const ourCLInput = process.argv.slice(2, process.argv.length); // capture CLI input from third index
-// console.log(ourCLInput);
+console.log('');
+const generatePage = () => 'Name: Andrew, Github: bwogihub';
+console.log(generatePage());
 
-const printOutCLInput = theArray => console.log(theArray); // arrow function to display it
+console.log('----------------------------------------------------------------');
 
-printOutCLInput(ourCLInput);
+const generateNewPage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generateNewPage('Andrew Bwogi','Bwogi'));
+console.log('');
 
+// multi-line strings
 
-/* history */
+const generateAnotherPage = (userName, githubName) => {
+   return `Name: ${userName} 
+Github: ${githubName}
+    `;
+};
 
-// const animalArray = ['dog','cat','cow'];
-// animalArray.push('pig');
+// console.log(generateAnotherPage('abwogi@ereports.net','bwogi'));
 
-// console.log(animalArray);
+const cLCapture = process.argv.slice(2, process.argv.length);
+// // instead of  
+// const theName = cLCapture[0];
+// const githubName = cLCapture[1];
 
-// const personalObj = {
-//     name: 'Bwogi',
-//     age: 99
-// }
+// use destructuring 
+let [theName, githubName] = cLCapture;
 
-// personalObj.age = 66;
-
-// personalObj.occupation = "Developer";
-
-// console.log(personalObj);
-
-
-// var transcriptObj = {
-//     id: '1101',
-//     studentId: '331313AS',
-//     status: 'partial'
-// }
-// transcriptObj.semID = '1';
-
-// console.log(transcriptObj);
-
-// console.log('---');
+console.log(theName, githubName);
+console.log(generateAnotherPage(theName, githubName));
 
 
+ const generateThisPage = (name, githubName) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio Demo</title>
+    </head>
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${githubName}">Github</a></h2>
+    </body>
+    </html>`
+};
 
-// // const message = 'Hello Node!';
-
-// // message = 'Goodbye node!'
-
-// // console.log(message);
-
-
-// // console.log('Hello Node!');
-// // console.log(document);
-// // var message = 'Hello Node';
+console.log(generateThisPage(theName, githubName));
 
 
 
-// // var sum = 3 + 4;
+// const ourCLInput = process.argv.slice(2, process.argv.length); // capture CLI input from third index
+// // console.log(ourCLInput);
 
-// // console.log(message);
-// // console.log(sum);
+// const printOutCLInput = theArray => console.log(theArray); // arrow function to display it
 
-// // var commandLineArgs = process.argv;
+// printOutCLInput(ourCLInput);
 
-// // console.log(commandLineArgs);
-// // // console.log(process.env);
-// // console.log(`process.argv[1]:  ${process.argv[1]}`);
-// // console.log(`process.argv[0]:  ${process.argv[0]}`);
-// // console.log(`process.argv:  ${process.argv}`);
-// // console.log('');
-// // const profileDataArgs = process.argv.slice(2, process.argv.length);
-// // // console.log(profileDataArgs);
+// console.log('----------------------------------------------------------------');
 
-// // const printProfileData = (profileDataArr) => {
-// //     console.log(profileDataArr);
+
+// /* history */
+
+// // const animalArray = ['dog','cat','cow'];
+// // animalArray.push('pig');
+
+// // console.log(animalArray);
+
+// // const personalObj = {
+// //     name: 'Bwogi',
+// //     age: 99
+// // }
+
+// // personalObj.age = 66;
+
+// // personalObj.occupation = "Developer";
+
+// // console.log(personalObj);
+
+
+// // var transcriptObj = {
+// //     id: '1101',
+// //     studentId: '331313AS',
+// //     status: 'partial'
+// // }
+// // transcriptObj.semID = '1';
+
+// // console.log(transcriptObj);
+
+// // console.log('---');
+
+
+
+// // // const message = 'Hello Node!';
+
+// // // message = 'Goodbye node!'
+
+// // // console.log(message);
+
+
+// // // console.log('Hello Node!');
+// // // console.log(document);
+// // // var message = 'Hello Node';
+
+
+
+// // // var sum = 3 + 4;
+
+// // // console.log(message);
+// // // console.log(sum);
+
+// // // var commandLineArgs = process.argv;
+
+// // // console.log(commandLineArgs);
+// // // // console.log(process.env);
+// // // console.log(`process.argv[1]:  ${process.argv[1]}`);
+// // // console.log(`process.argv[0]:  ${process.argv[0]}`);
+// // // console.log(`process.argv:  ${process.argv}`);
+// // // console.log('');
+// // // const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // // // console.log(profileDataArgs);
+
+// // // const printProfileData = (profileDataArr) => {
+// // //     console.log(profileDataArr);
+// // // }
+
+// // // printProfileData(profileDataArgs);
+
+
+// // // arrow functions 
+// // // old way of doing things
+// // function addNums(numOne, numTwo){
+// //     return numTwo + numOne;
+// // }
+
+// // // new ways 
+// // const addNumbers = function(numOne, numTwo){
+// //     return numOne + numTwo;
+// // }
+
+// // // another new way 
+// // const addTheNos = (numOne, numTwo) => {
+// //     return numTwo + numOne;
+// // }
+
+// // // they can all be rewritten
+// // const addThoseNos = (numOne, numTwo) => numTwo + numOne;
+
+// // console.log(`addNums: ${addNums(2,1)}`);
+// // console.log(`addNumbers: ${addNumbers(2,1)}`);
+// // console.log(`addTheNos: ${addTheNos(2,1)}`);
+// // console.log(`addThoseNos: ${addThoseNos(2,1)}`);
+// // console.log('---');
+// // console.log('---');
+
+// // const profileDataArgs = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+// // console.log(profileDataArgs);
+// // console.log('---');
+
+// // const profileDataArgsNew = process.argv.slice(0, 2); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+// // console.log(profileDataArgsNew);
+// // console.log('---');
+
+// // const profileDataArgsNewOne = process.argv.slice(0, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+// // console.log(profileDataArgsNewOne);
+// // console.log('---');
+
+// // const printProfileData = profileDataArr => {
+// //     for (let i = 0; i < profileDataArr.length; i++) {
+// //        console.log(profileDataArr[i]);  
+// //     }
 // // }
 
 // // printProfileData(profileDataArgs);
 
+// // console.log('---');
 
-// // arrow functions 
-// // old way of doing things
-// function addNums(numOne, numTwo){
-//     return numTwo + numOne;
-// }
+// // const profile = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
+// // console.log(profile);
+// // console.log('---');
 
-// // new ways 
-// const addNumbers = function(numOne, numTwo){
-//     return numOne + numTwo;
-// }
-
-// // another new way 
-// const addTheNos = (numOne, numTwo) => {
-//     return numTwo + numOne;
-// }
-
-// // they can all be rewritten
-// const addThoseNos = (numOne, numTwo) => numTwo + numOne;
-
-// console.log(`addNums: ${addNums(2,1)}`);
-// console.log(`addNumbers: ${addNumbers(2,1)}`);
-// console.log(`addTheNos: ${addTheNos(2,1)}`);
-// console.log(`addThoseNos: ${addThoseNos(2,1)}`);
-// console.log('---');
-// console.log('---');
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
-// console.log(profileDataArgs);
-// console.log('---');
-
-// const profileDataArgsNew = process.argv.slice(0, 2); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
-// console.log(profileDataArgsNew);
-// console.log('---');
-
-// const profileDataArgsNewOne = process.argv.slice(0, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
-// console.log(profileDataArgsNewOne);
-// console.log('---');
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//        console.log(profileDataArr[i]);  
-//     }
-// }
-
-// printProfileData(profileDataArgs);
-
-// console.log('---');
-
-// const profile = process.argv.slice(2, process.argv.length); // slice('start from index' , 'end at index') in other words, skip the first 2 indexes 
-// console.log(profile);
-// console.log('---');
-
-// const printProfile = theArray => console.log(theArray);
+// // const printProfile = theArray => console.log(theArray);
 
 
-// printProfile(profile);
+// // printProfile(profile);
 
-// console.log('---');
-// console.log('---');
+// // console.log('---');
+// // console.log('---');
 
-// // cleaner loops
+// // // cleaner loops
 
-// const printProfileDataNew = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++){
-//         profileDataArr[i];
-//     }
-//     console.log('---');
+// // const printProfileDataNew = profileDataArr => {
+// //     for (let i = 0; i < profileDataArr.length; i++){
+// //         profileDataArr[i];
+// //     }
+// //     console.log('---');
 
-//     // is the same as
+// //     // is the same as
     
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem);
-//     });
+// //     profileDataArr.forEach((profileItem) => {
+// //         console.log(profileItem);
+// //     });
 
-// };
+// // };
 
 
 
